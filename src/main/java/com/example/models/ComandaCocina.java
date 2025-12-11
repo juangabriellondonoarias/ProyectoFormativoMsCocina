c2413ddd017ff0ab93419727393437c106d3fac2
Carlos Andres Mnedez <camendez2515@gmail.com>
2025-12-11 15:07:19 -0500

    se avanza en ComandaCocina

---
+++ src/main/java/com/example/models/ComandaCocina.java
@@ -0,0 +1,107 @@
+package com.example.models;
+
+import java.time.LocalDateTime; 
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import com.example.models.EstadoComanda;     
+import com.example.models.PrioridadComanda;
+
+@Entity
+
+@Table(name = "COMANDA_COCINA")
+public class ComandaCocina {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_comanda_cocina")
+	private Integer idComandaCocina;
+	
+	@Column(name = "id_comanda_restaurante", nullable = false)
+	private Integer idComandaRestaurante;
+	
+	@Column(name = "id_mesa" , nullable = false)
+	private Integer idMesa;
+	
+	 /*mapeo del datetime*/
+	
+	@CreationTimestamp
+	@Column(name = "hora_entrada" , nullable = false , updatable = false)
+	private LocalDateTime horaEntrada;
+	
+	/*mapeo del enum estado*/
+	/* Mapeo del ENUM ESTADO (Faltaba) */
+    @Enumerated(EnumType.STRING)
+    @Column(name = "estado", nullable = false)
+    private EstadoComanda estado = EstadoComanda.EN_ESPERA; 
+	
+    /* Mapeo del enum prioridad */
+	@Enumerated(EnumType.STRING)
+	@Column(name = "prioridad" , nullable = false) 
+	private PrioridadComanda prioridad = PrioridadComanda.MEDIA;
+	/*conatructor*/
+	
+	public ComandaCocina() {
+		
+	}
+	
+	/*get y set*/
+	
+	public Integer getIdPedidoCocina() {
+		return idComandaCocina;
+	}
+	
+	public Integer getIdPedidoRestaurante() {
+		return idComandaRestaurante;
+		
+	}
+	
+	public Integer getIdMesa () {
+		return idMesa;
+	}
+	
+	public LocalDateTime getHoraEntrada () {
+		return horaEntrada;
+	}
+	
+	public EstadoComanda getEstado () {
+		return estado;
+	}
+	
+	public PrioridadComanda getPrioridad () {
+		return prioridad;
+	}
+	
+	public void setIdComandaCocina (Integer idComandaCocina) {
+		this.idComandaCocina = idComandaCocina;
+	}
+	
+	public void setIComandaRestaurante(Integer idPedidoRestaurante) {
+		this.idComandaRestaurante = idComandaRestaurante;
+	}
+	
+	public void setIdMesa(Integer idmesa) {
+		this.idMesa = idMesa;
+	}
+	
+	public void setHoraEntrada(LocalDateTime horaEntrada ) {
+		this.horaEntrada = horaEntrada;
+	}
+	
+	public void setEstadoComanda( EstadoComanda estado) {
+		this.estado = estado;
+	}
+	
+	public void setPrioridadComanda(PrioridadComanda prioridad) {
+		this.prioridad = prioridad;
+	}
+	
+}
---
+++ src/main/java/com/example/models/EstadoComanda.java
@@ -0,0 +1,9 @@
+package com.example.models;
+
+public enum EstadoComanda {
+	
+	EN_ESPERA,
+	EN_PREPARACION,
+	LISTO,
+	ENTREGADO
+}
